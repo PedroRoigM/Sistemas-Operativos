ad02d4195806102e291ce68c3b30bfd56a61ca0c
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2023-11-07 11:03:31 +0100

    Esquema

---
+++ Proyecto_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM_LINEA 3
+
+typedef struct {
+        unsigned char ETQ;
+        unsigned char Data[TAM_LINEA];
+}T_CACHE_LINE;
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
+void VolcarCACHE(T_CACHE_LINE *tbl);
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
+
+
+
+void main(){
+
+
+}
