ace4a4477d9e54ddecd4eedef15e7b1d8eb6df86
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2023-11-07 11:35:58 +0100

    Prototipo
    Declaración de las funciones obligatorias para el proyecto, comentarios con los datos necesarios y declaración de la estructura y el array de estructuras.

--- Proyecto_1.c
+++ Proyecto_1.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define TAM_LINEA 3
-
+#define TAM_LINEA 8 //3 bits de linea 2^3 = 8
+double globaltime = 0;
+int numfallos = 0;
 typedef struct {
         unsigned char ETQ;
         unsigned char Data[TAM_LINEA];
@@ -12,9 +13,27 @@ void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
 
+//Bus de memoria de 12 bits y usa memoria fisica
+//Caché de 8 lineas con correspondencia directa y 16 bytes por linea
+//palabra(4 bits), linea(3 bits), etiqueta(5 bits)
 
+int main(){
+	T_CACHE_LINE MEMsym[16];
 
-void main(){
+	unsigned char Simul_RAM[4096];
 
+	return 0;
+}
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+	//aqui en teoria hay que inicializar el array de estructuras como pone en el pdf
+}
+void VolcarCACHE(T_CACHE_LINE *tbl){
+	//tiene de mostrar por pantalla los datos
+}
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque){
 
 }
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque){
+	printf("T: %d, Fallo de CACHE %d, ADDR %04X Label %X linea %02X palabra %02X bloque %02X");
+}
+
