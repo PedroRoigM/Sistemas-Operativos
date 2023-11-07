cafc2f9009480ac5a48d5aa27e174b3d84738365
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2023-11-07 16:41:00 +0100

    Actualizacion Volcar Cache
    Imprime todas las etiquetas y data en hexadecimal

--- VolcarCache.c
+++ VolcarCache.c
@@ -3,12 +3,11 @@
 
 void VolcarCACHE(T_CACHE_LINE *tbl){
 	int i = 0;
-	printf("T: , Acierto de CACHE, ADDR, ETQ  linea  palabra  DATO \n");
-	for(int i = 8; i > 0; i--){
+	for(int i = NUM_FILAS - 1; i >= 0; i--){ 
 
-		printf("ETQ:%d Data ", tbl[i].ETQ);
-		for(int j = 15; j > 0; j--){
-			printf("%d ", tbl[i].Data[j]);
+		printf("ETQ:%X Data ", tbl[i].ETQ);
+		for(int j = TAM_LINEA; j >= 0; j--){
+			printf("%X ", tbl[i].Data[j]);
 		}
 		printf("\n");
 	}
