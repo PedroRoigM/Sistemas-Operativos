bfbb455aed635e9323009b313e334b59556b5b8a
iviguio <105162173+iviguio@users.noreply.github.com>
2023-11-28 20:07:34 +0100

    Update TratarFallo.c
    tratar fallo arreglado

--- TratarFallo.c
+++ TratarFallo.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 #include"funciones.h"
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque){
-	int *variablesG = tami(20, 1);
-	//numfallos++;
-	printf("T: %d, Fallo de caché %d, addr %04X etq %X linea %02X palabra %02X bloque %02X", variablesG[0], variablesG[1], *MRAM, ETQ, linea, bloque, 1);
-	//globaltime+=20;
+    int startAddress = bloque * TAM_LINEA;  
+    for (int i = 0; i < TAM_LINEA; i++) {
+        tbl[linea].Data[i] = MRAM[startAddress + i];
+    }
+    tbl[linea].ETQ = ETQ;
 }
+
