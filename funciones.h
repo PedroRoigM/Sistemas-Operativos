9b79ff5cdc5101988288c43c9a88001fcf02c8cf
iviguio <i.guio.aunon@gmail.com>
2023-12-02 19:59:11 +0100

    borrado cargarRam
    esta funcion esta implementada en el main y no la estamos llamando.

--- funciones.h
+++ funciones.h
@@ -17,7 +17,6 @@ void LimpiarCACHE(T_CACHE_LINE tbl[]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
-int CargarRam(unsigned char *Simul_RAM);
 void MostrarCache(T_CACHE_LINE *cache);
 
 #endif
